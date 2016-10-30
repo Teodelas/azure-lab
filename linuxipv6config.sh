b440ca229adb7f20ba4fabf7c2a469ede837e7e3
Teodelas <teodelas@microsoft.com>
2016-10-29 20:35:03 -0400

    Update linuxipv6config.sh

--- linuxipv6config.sh
+++ linuxipv6config.sh
@@ -4,4 +4,4 @@ sed -i 's/IPV6INIT=no//g' /etc/sysconfig/network-scripts/ifcfg-eth0
 echo -e "\nNETWORKING_IPV6=yes" >> /etc/sysconfig/network
 echo -e "\nIPV6INIT=yes" >>  /etc/sysconfig/network-scripts/ifcfg-eth0
 echo -e "\nDHCPV6C=yes" >>  /etc/sysconfig/network-scripts/ifcfg-eth0
-reboot
+shutdown -r +2 "Rebooting to apply IPv6 changes."
