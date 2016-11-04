88019cc07f16f437b5585aa1508da1fdb604d963
Teodelas <teodelas@microsoft.com>
2016-11-03 20:35:35 -0400

    Update buildrdgw.ps1

--- buildrdgw.ps1
+++ buildrdgw.ps1
@@ -1,5 +1,15 @@
  Param(
   [string]$FQDN
 )
+ Add-WindowsFeature -Name RDS-Gateway -IncludeAllSubFeature
+ Add-WindowsFeature -Name 
+ Import-Module RemoteDesktopServices
+ CD RDS:\GatewayServer\CAP
+ new-item -Name RDGWCAP -UserGroups 'Administrators@BUILTIN' -AuthMethod 1
+ CD RDS:\GatewayServer\RAP
+ new-item -Name RDGWRAP -UserGroups 'Administrators@BUILTIN' -ComputerGroupType 2
 
-$FQDN | Out-File "c:\users\public\test.txt"
+ $cert= New-SelfSignedCertificate -CertStoreLocation cert:\localmachine\my -DnsName $FQDN
+
+ Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $cert.Thumbprint
+ get-childitem cert:\localmachine\my | where-object { $_.Subject -eq "CN=$FQDN" } | Export-Certificate -FilePath 'c:\users\rdgw.cer'
