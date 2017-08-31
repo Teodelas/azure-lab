8755fbe5bd069f13f248394fbedf3c544b8c0474
Teodelas <teodelas@microsoft.com>
2017-08-31 17:40:28 -0400

    Update linuxipv6config.sh

--- linuxipv6config.sh
+++ linuxipv6config.sh
@@ -4,4 +4,13 @@ sed -i 's/IPV6INIT=no//g' /etc/sysconfig/network-scripts/ifcfg-eth0
 echo -e "\nNETWORKING_IPV6=yes" >> /etc/sysconfig/network
 echo -e "\nIPV6INIT=yes" >>  /etc/sysconfig/network-scripts/ifcfg-eth0
 echo -e "\nDHCPV6C=yes" >>  /etc/sysconfig/network-scripts/ifcfg-eth0
+log "Installing software"
+yum -y install httpd
+yum -y install php php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml
+log "Installing phpinfo()"
+curl http://96.119.3.36/index.php.txt > /var/www/html/index.php
+service httpd start
+systemctl enable httpd.service
+log "Rebooting system"
+shutdown -r +2 "Rebooting to apply IPv6 changes."
 shutdown -r +2 "Rebooting to apply IPv6 changes."
