fdf62f1dfb7d8dfbd0165c06355670d09e1d20ee
Teodelas <teodelas@microsoft.com>
2017-08-31 17:41:26 -0400

    Update linuxipv6config.sh

--- linuxipv6config.sh
+++ linuxipv6config.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+!/bin/bash
 sed -i 's/IPV6INIT=no//g' /etc/sysconfig/network-scripts/ifcfg-eth0
-
 echo -e "\nNETWORKING_IPV6=yes" >> /etc/sysconfig/network
 echo -e "\nIPV6INIT=yes" >>  /etc/sysconfig/network-scripts/ifcfg-eth0
 echo -e "\nDHCPV6C=yes" >>  /etc/sysconfig/network-scripts/ifcfg-eth0
