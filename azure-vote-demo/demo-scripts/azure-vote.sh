cd49e406912a6a55be919bcf160f7d845f925612
teodelas <teodelas@microsoft.com>
2018-07-02 08:50:54 -0400

    azure-vote

--- azure-vote-demo/demo-scripts/azure-vote.sh
+++ azure-vote-demo/demo-scripts/azure-vote.sh
@@ -4,4 +4,9 @@ az vm start -g Azure_Demos -n centosvm
 
 ssh teodelas@teolinuxvm.eastus.cloudapp.azure.com
 #on centosvm:
+sudo systemctl start docker
+cd azure-lab
+git pull
+cd azure-vote-demo 
+docker build . 
 
